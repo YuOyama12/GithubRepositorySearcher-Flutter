825324aad0077d0929c220249e7552cb2f9b2e2d
YuOyama12 <yuoyama.5@gmail.com>
2025-08-06 23:01:37 +0900

    自動生成パッケージのバージョンを変更したことによる差分を反映

--- lib/domain/entity/response/repositories_response/repositories_response.g.dart
+++ lib/domain/entity/response/repositories_response/repositories_response.g.dart
@@ -7,17 +7,17 @@ part of 'repositories_response.dart';
 // **************************************************************************
 
 _RepositoriesResponse _$RepositoriesResponseFromJson(
-        Map<String, dynamic> json) =>
-    _RepositoriesResponse(
-      totalCount: (json['total_count'] as num).toInt(),
-      items: (json['items'] as List<dynamic>)
-          .map((e) => Repository.fromJson(e as Map<String, dynamic>))
-          .toList(),
-    );
+  Map<String, dynamic> json,
+) => _RepositoriesResponse(
+  totalCount: (json['total_count'] as num).toInt(),
+  items: (json['items'] as List<dynamic>)
+      .map((e) => Repository.fromJson(e as Map<String, dynamic>))
+      .toList(),
+);
 
 Map<String, dynamic> _$RepositoriesResponseToJson(
-        _RepositoriesResponse instance) =>
-    <String, dynamic>{
-      'total_count': instance.totalCount,
-      'items': instance.items,
-    };
+  _RepositoriesResponse instance,
+) => <String, dynamic>{
+  'total_count': instance.totalCount,
+  'items': instance.items,
+};
