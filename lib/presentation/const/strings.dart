cc53495dd52605b58adc57fb72976573752e859f
YuOyama12 <yuoyama.5@gmail.com>
2025-08-08 22:02:56 +0900

    リポジトリ検索用の入力欄を追加

---
+++ lib/presentation/const/strings.dart
@@ -0,0 +1,6 @@
+class StringConsts {
+  static const String homePageTitle = 'GithubRepositorySearcher';
+  static const String searchPlaceholder = 'キーワードを入力';
+  static const String search = '検索';
+
+}
--- lib/presentation/ui/home_page.dart
+++ lib/presentation/ui/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:github_repository_searcher/presentation/const/strings.dart';
+import 'package:github_repository_searcher/presentation/provider/repository_query_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class HomePage extends HookConsumerWidget {
@@ -6,16 +8,43 @@ class HomePage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final query = ref.read(repositoryQueryProvider);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Flutter Demo Home Page'),
+        title: Text(StringConsts.homePageTitle),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsetsGeometry.symmetric(
+                vertical: 6.0,
+                horizontal: 12.0
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: TextField(
+                      controller: TextEditingController(text: query),
+                      decoration: InputDecoration(
+                        hintText: StringConsts.searchPlaceholder,
+                      ),
+                      onChanged: (newValue) {
+                        ref.read(repositoryQueryProvider.notifier).state = newValue;
+                      },
+                    )
+                ),
+                FilledButton(
+                  onPressed: () {
+                  },
+                  child: Text(StringConsts.search),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
