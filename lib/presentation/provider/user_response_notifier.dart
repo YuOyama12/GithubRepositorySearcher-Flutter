31736be09e297ac7f029d0cc95b8343911d58653
YuOyama12 <yuoyama.5@gmail.com>
2025-08-25 21:23:43 +0900

    ユーザー情報取得APIをつなぎこみ

---
+++ lib/presentation/provider/user_response_notifier.dart
@@ -0,0 +1,38 @@
+import 'package:github_repository_searcher/data/repository/user_repository.dart';
+import 'package:github_repository_searcher/domain/entity/response/user_response/user_response.dart';
+import 'package:github_repository_searcher/presentation/provider/loading_progress_controller.dart';
+import 'package:hooks_riverpod/legacy.dart';
+
+final userResponseNotifierProvider =
+StateNotifierProvider<UserResponseNotifier, UserResponse?>(
+      (ref) {
+    final userRepository = ref.watch(userRepositoryProvider);
+    final loadingController = ref.read(loadingProgressController.notifier);
+
+    return UserResponseNotifier(
+      loadingController: loadingController,
+      userRepository: userRepository,
+    );
+  },
+);
+
+class UserResponseNotifier extends StateNotifier<UserResponse?> {
+  final LoadingProgressController loadingController;
+  final UserRepository userRepository;
+
+  UserResponseNotifier({
+    required this.loadingController,
+    required this.userRepository,
+  }) : super(null);
+
+  Future<void> fetchUserById({
+    required int userId
+  }) async {
+    try {
+      loadingController.setLoading(isLoading: true);
+      state = await userRepository.fetchUserById(userId: userId);
+    } finally {
+      loadingController.setLoading(isLoading: false);
+    }
+  }
+}
--- lib/presentation/ui/user_detail/user_detail_page.dart
+++ lib/presentation/ui/user_detail/user_detail_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:github_repository_searcher/presentation/ui/user_detail/navigation/user_detail_args.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../provider/user_response_notifier.dart';
 
 class UserDetailPage extends HookConsumerWidget {
   const UserDetailPage({
@@ -12,10 +15,21 @@ class UserDetailPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final userResponse = ref.watch(userResponseNotifierProvider);
+
+    useEffect(() {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        ref.read(userResponseNotifierProvider.notifier)
+            .fetchUserById(userId: args.userId);
+      });
+
+      return null;
+    }, []);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(args.userId.toString()),
+        title: Text(userResponse?.login ?? ''),
       ),
       body: Column(
         children: [
