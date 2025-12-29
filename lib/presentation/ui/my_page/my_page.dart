5ea5d7c95a390e789958bcdce7e4b7e6b3f3db91
YuOyama12 <yuoyama.5@gmail.com>
2025-12-29 09:51:31 +0900

    マイページのUIを簡易的に作成

--- lib/presentation/ui/my_page/my_page.dart
+++ lib/presentation/ui/my_page/my_page.dart
@@ -1,11 +1,19 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../const/strings.dart';
 
 class MyPage extends HookConsumerWidget {
   const MyPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return SizedBox.expand();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(StringConsts.myPage),
+      ),
+      body: Column(children: [Text(StringConsts.myPage)]),
+    );
   }
 }
